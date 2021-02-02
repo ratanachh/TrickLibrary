0573032bd494ea5c39f29c455a179466538465c5
Ratana <chhormratana@gmail.com>
2021-02-02 13:20:18 +0700

    first commit

---
+++ TrickLibrary.Api/Controllers/Home.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrickLibrary.Api.Controllers
+{
+    [ApiController]
+    [Route("api/home")]
+    public class Home : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "hello world";
+        }
+    }
+}
